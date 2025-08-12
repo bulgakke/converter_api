f54ebc3274d30d51f2000674a53a0b504ee8a47b
bulgakke <be.evgeniy.bulavin@gmail.com>
2025-08-13 02:10:25 +0400

    feat: svg_content normalization

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,4 +1,13 @@
 class Document < ApplicationRecord
+  # Doesn't matter for generating PDFs, but helps with caching
+  # Not perfect (e. g. viewBox="0 0 1024 1024" and viewBox="0.0 0.0 1024.0 1024.0")
+  # will still turn out different
+  normalizes :svg_content, with: ->(value) {
+    value.gsub(/<!--.*?-->/m, "") # remove comments
+      .gsub(/\n+/, "") # remove newlines
+      .squish # remove extra whitespace
+  }
+
   validates :svg_content, presence: true
 
   has_one_attached :pdf_file
--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -20,4 +20,42 @@ RSpec.describe Document, type: :model do
       end
     end
   end
+
+  describe "normalizations" do
+    describe "svg_content" do
+      let(:raw_svg) do
+        <<~SVG
+          <!-- comment -->
+          <svg width="100" height="100">
+            <rect x="10" y="10" width="80" height="80"/>
+          </svg>
+
+        SVG
+      end
+
+      let(:expected_svg) do
+        '<svg width="100" height="100"> <rect x="10" y="10" width="80" height="80"/></svg>'
+      end
+
+      it "removes comments, newlines and squishes whitespace" do
+        expect(Document.normalize_value_for(:svg_content, raw_svg)).to eq(expected_svg)
+      end
+
+      it "does not alter already normalized svg_content" do
+        expect(Document.normalize_value_for(:svg_content, expected_svg)).to eq(expected_svg)
+      end
+
+      it "does not change blank strings" do
+        expect(Document.normalize_value_for(:svg_content, "")).to eq("")
+      end
+
+      it "does not break on other types" do
+        expect(Document.normalize_value_for(:svg_content, "1")).to eq("1")
+      end
+
+      it "leaves nil as nil" do
+        expect(Document.normalize_value_for(:svg_content, nil)).to be_nil
+      end
+    end
+  end
 end
